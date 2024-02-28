e27f51acc2f61477f0473ae06764caedc6c0070d
juanesgv <juanesgarviedma23@gmail.com>
2024-02-28 12:04:06 -0500

    configuración inicial con next

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='py-8 flex flex-col md:flex-row items-center justify-between'>
+      <img className='w-64 mb-8 md:0' src='logo.svg'/>
+    </div>
+  )
+}
+
+export default Header
---
+++ components/Layout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Head from "next/head";
+import Header from "./Header";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>NodeSend</title>
+      </Head>
+
+      <Header/>
+
+      <div className="bg-gray-100 min-h-screen">
+        <div className="container mx-auto">
+          <main className="mt-20">{children}</main>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
