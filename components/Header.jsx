32ec7107a1c2a2a892d86f5e55ceb75f68df538e
juanesgv <juanesgarviedma23@gmail.com>
2024-02-28 16:11:56 -0500

    Creación de las páginas + routing

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
-  return (
-    <div className='py-8 flex flex-col md:flex-row items-center justify-between'>
-      <img className='w-64 mb-8 md:0' src='logo.svg'/>
-    </div>
-  )
+    return (
+        <div className='py-8 flex flex-col md:flex-row items-center justify-between'>
+            <Link href="/">
+                <img className='w-64 mb-8 md:0' src='logo.svg' />
+            </Link>
+            <div className='flex gap-3'>
+                <Link href="/login" className='bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase'>
+                    Iniciar sesión
+                </Link>
+                <Link href="/crear_cuenta" className='bg-black px-5 py-3 rounded-lg text-white font-bold uppercase'>
+                    Crear cuenta
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default Header
---
+++ pages/crear_cuenta.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '@/components/Layout'
+
+const crear_cuenta = () => {
+    return (
+        <Layout>
+            Crear cuenta
+        </Layout>
+    )
+}
+
+export default crear_cuenta
